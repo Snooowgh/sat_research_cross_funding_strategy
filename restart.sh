d0fcc5851f95fc591844f06fbea002431855fe87
Snooowgh <37729805+Snooowgh@users.noreply.github.com>
2025-10-11 18:30:54 +0800

    deploy

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export PATH=/Applications/Docker.app/Contents/Resources/bin:$PATH
 docker compose up -d
-docker compose restart arbitrage_monitor
-docker compose logs -f --tail=10 arbitrage_monitor
+docker compose restart arbitrage_process
+docker compose logs -f --tail=10 arbitrage_process
